e8d947f5eb8fda4fabd9631a6383c5ce82e48b82
Ho9tocraft <ho9tocraft222@gmail.com>
2026-08-10 20:38:29 +0900

    Add: Full Faithial Parallel Rapid Assembly Line

--- kubejs/startup_scripts/_initial.js
+++ kubejs/startup_scripts/_initial.js
@@ -1,2 +1,128 @@
 // priority: 999
-Platform.mods.kubejs.name = "Fortresscraft GTCEu";
+Platform.setModName('kubejs', 'Fortresscraft GTCEu'); // Mod Name
+
+/**
+ * @typedef {{
+ *   mode: 'normal'|'extreme'|'savage'|'ultimate',
+ *   indebug: boolean,
+ *   forced_options: {
+ *     _comment_bypass_shishamotech_disabling: string,
+ *     bypass_shishamotech_disabling: boolean,
+ *     _comment_disable_mbd2_mekanism_multiblock: string,
+ *     disable_mbd2_mekanism_multiblock: boolean,
+ *     _comment_disable_mbd2_botania_multiblock: string,
+ *     disable_mbd2_botania_multiblock: boolean
+ *   }
+ * }} FGTCEuConfig
+ */
+
+/* ---- Mod Integrations ---- */
+global.loadedMods = {
+  SHISHAMO_TECH: Platform.isLoaded('shishamo_tech'), // Shishamo Tech
+  HOSTILE_NEURAL_NETWORKS: Platform.isLoaded('hostilenetworks'), // Hostile Neural Networks
+};
+
+/* ---- Modpack Config ---- */
+const configName = 'config/packmode.json';
+/**
+ * default config
+ * @type {FGTCEuConfig}
+ */
+const defaultConfig = {
+  mode: 'normal', // Normal, Extreme(極), Savage(零式), Ultimate(絶)
+  indebug: false,
+  forced_options: {
+    // forced options
+    _comment_bypass_shishamotech_disabling: 'Enable Bypassing ST-related disabling. If not Normal or Extreme, this feature is always enable.',
+    bypass_shishamotech_disabling: false, // マルチブロック機械(大型・パラレルとMEアクセスハッチ対応のアセンブリラインなど)
+    _comment_disable_mbd2_mekanism_multiblock: 'Disable Multiblocked 2 Mekanism Multiblock.',
+    disable_mbd2_mekanism_multiblock: false, // Mek用マルチブロック機械(マルチブロック回転式流体凝縮機など)
+    _comment_disable_mbd2_botania_multiblock: 'Disable Multiblocked 2 Botania Multiblock.',
+    disable_mbd2_botania_multiblock: false, // Botania用マルチブロック機械(マルチブロックマナ生成など)
+  }
+};
+
+const rawConfig = JsonIO.read(configName);
+/**
+ * config
+ * @type {FGTCEuConfig}
+ */
+let config = {
+  mode: String(rawConfig.get('mode')),
+  indebug: Boolean(rawConfig.get('indebug')),
+  forced_options: {
+    bypass_shishamotech_disabling: Boolean(rawConfig.get('forced_options') ? rawConfig.get('forced_options').get('bypass_shishamotech_disabling') : defaultConfig.forced_options.bypass_shishamotech_disabling),
+    disable_mbd2_mekanism_multiblock: Boolean(rawConfig.get('forced_options') ? rawConfig.get('forced_options').get('disable_mbd2_mekanism_multiblock') : defaultConfig.forced_options.disable_mbd2_mekanism_multiblock),
+    disable_mbd2_botania_multiblock: Boolean(rawConfig.get('forced_options') ? rawConfig.get('forced_options').get('disable_mbd2_botania_multiblock') : defaultConfig.forced_options._comment_disable_mbd2_botania_multiblock),
+  }
+};
+let initStart = false;
+if (!rawConfig) {
+  JsonIO.write(configName, defaultConfig);
+  console.warn(`Created new ${configName}`);
+  config = defaultConfig;
+  initStart = true; // バリデーション回避
+}
+
+// configバリデーション
+if (!initStart) {
+  // タイプバリデーション
+  console.log('TYPE VALIDATION start.');
+  if (typeof config.mode !== 'string' || typeof config.indebug !== 'boolean' ||
+    typeof config.forced_options.bypass_shishamotech_disabling !== 'boolean' ||
+    typeof config.forced_options.disable_mbd2_mekanism_multiblock !== 'boolean' ||
+    typeof config.forced_options.disable_mbd2_botania_multiblock !== 'boolean') {
+    // いずれかのタイプバリデーション失敗
+    if (typeof config.mode !== 'string') {
+      console.error(`SYNTAX ERROR DETECTED: config.mode is not string, found: ${typeof config.mode}`);
+      config.mode = defaultConfig.mode;
+    }
+    if (typeof config.indebug !== 'boolean') {
+      console.error(`SYNTAX ERROR DETECTED: config.indebug is not boolean, found: ${typeof config.indebug}`);
+      config.indebug = defaultConfig.indebug;
+    }
+    if (typeof config.forced_options.bypass_shishamotech_disabling !== 'boolean') {
+      const erroredSTL = config.forced_options.bypass_shishamotech_disabling;
+      console.error(`SYNTAX ERROR DETECTED: config.forced_options.bypass_shishamotech_disabling is not boolean, found: ${typeof erroredSTL}`);
+      config.forced_options.bypass_shishamotech_disabling = defaultConfig.forced_options.bypass_shishamotech_disabling;
+    }
+    if (typeof config.forced_options.disable_mbd2_mekanism_multiblock !== 'boolean') {
+      const erroredDMM = config.forced_options.disable_mbd2_mekanism_multiblock;
+      console.error(`SYNTAX ERROR DETECTED: config.forced_options.disable_mbd2_mekanism_multiblock is not boolean, found: ${typeof erroredDMM}`);
+      config.forced_options.disable_mbd2_mekanism_multiblock = defaultConfig.forced_options.disable_mbd2_mekanism_multiblock;
+    }
+    if (typeof config.forced_options.disable_mbd2_botania_multiblock !== 'boolean') {
+      const erroredDBM = config.forced_options.disable_mbd2_botania_multiblock;
+      console.error(`SYNTAX ERROR DETECTED: config.forced_options.disable_mbd2_botania_multiblock is not string, found: ${typeof erroredDBM}`);
+      config.forced_options.disable_mbd2_botania_multiblock = defaultConfig.forced_options.disable_mbd2_botania_multiblock;
+    }
+  }
+  else {
+    // タイプバリデーション通過、モード指定バリデーション
+    console.log('TYPE VALIDATION passed, MODE SELECT VALIDATION start.');
+    switch (config.mode) {
+      case 'normal':
+        break;
+      case 'extreme':
+      case 'savage':
+      case 'ultimate':
+        const modeText = `${config.mode.charAt(0).toUpperCase()}${config.mode.slice(1)}`;
+        console.warn(`WIP: Fortresscraft GTCEu (${modeText}) is now Work in Progress, fallbacking to Normal.`);
+        config.mode = 'normal';
+        break;
+      default:
+        console.error(`SYNTAX ERROR DETECTED: config.mode is invalid, overwrote. Current: ${config.mode}`);
+        config.mode = defaultConfig.mode;
+        break;
+    }
+  }
+}
+
+// 適用
+global.packFeatures = {
+  mode: config.mode,
+  debug: config.indebug,
+  bypassSTDisable: (config.mode === 'normal' || config.mode === 'extreme') ? config.forced_options.bypass_shishamotech_disabling : true,
+  disableMBD2Mek: config.forced_options.disable_mbd2_mekanism_multiblock,
+  disableMBD2Bot: config.forced_options.disable_mbd2_botania_multiblock,
+};
---
+++ kubejs/startup_scripts/fgtceu/multiblock_extras.js
@@ -0,0 +1,72 @@
+/* ---- Loaded Mods based Multiblocks ---- */
+GTCEuStartupEvents.registry('gtceu:machine', event => {
+  const { loadedMods, packFeatures, FGTCEuCommonStartupFunctions } = global;
+  const { outTranslatableString } = FGTCEuCommonStartupFunctions;
+  if (!loadedMods.SHISHAMO_TECH || packFeatures.bypassSTDisable) {
+    // Full-faithial Parallel Rapid Assembly Line
+    // 完全並列型高速組立ライン
+    event.create('ffaithial_prpd_assembly_line', 'multiblock')
+      .rotationState(RotationState.ALL)
+      .recipeTypes('assembler', 'circuit_assembler', 'assembly_line')
+      .recipeModifiers([
+        GTRecipeModifiers.OC_NON_PERFECT_SUBTICK,
+        GTRecipeModifiers.BATCH_MODE,
+        GTRecipeModifiers.PARALLEL_HATCH,
+        GTRecipeModifiers.DEFAULT_ENVIRONMENT_REQUIREMENT,
+      ])
+      .appearanceBlock(GCYMBlocks.CASING_LARGE_SCALE_ASSEMBLING)
+      .pattern(definition => FactoryBlockPattern.start()
+        .aisle('nnHHHnn','nHHHHHn','HHHHHHH','HHHHHHH','HHHHHHH','nHHHHHn','nnHHHnn')
+        .aisle('nCHHHCn','CChPhCC','Hh G hH','Hh   hH','Hh R hH','CCGMGCC','nCHHHCn')
+        .aisle('nnHHHnn','nvhPhvn','Hh G hH','L     L','Hh R hH','nvGMGvn','nnHHHnn')
+        .aisle('nCHHHCn','CChPhCC','Hh G hH','L     L','Hh R hH','ChGMGhC','nCHHHCn')
+        .aisle('nnHHHnn','nHhPhHn','Hh G hH','L     L','H  R  H','nhGMGhn','nnHHHnn')
+        .aisle('nnHHHnn','nHhPhHn','Hh G hH','L     L','H  R  H','nhGMGhn','nnHHHnn')
+        .aisle('nnHHHnn','nHhPhHn','Hh G hH','L     L','H  R  H','nhGMGhn','nnHHHnn')
+        .aisle('nnHHHnn','nHhPhHn','Hh G hH','L     L','H  R  H','nhGMGhn','nnHHHnn')
+        .aisle('nnHHHnn','nHhPhHn','Hh G hH','L     L','H  R  H','nhGMGhn','nnHHHnn')
+        .aisle('nCHHHCn','CChPhCC','Hh G hH','L     L','Hh R hH','ChGMGhC','nCHHHCn')
+        .aisle('nnHHHnn','nVhPhVn','Hh G hH','L     L','Hh R hH','nVGMGVn','nnHHHnn')
+        .aisle('nCHHHCn','CChPhCC','HhhGhhH','L     L','HhhRhhH','CCGMGCC','nCHHHCn')
+        .aisle('nnHHHnn','nCCCCCn','HChhhCH','HCLLLCH','HCCRCCH','nCCCCCn','nnHHHnn')
+        .aisle('nnnGnnn','nnGCGnn','nGCCCGn','GCWWWCG','nGCRCGn','nnGCGnn','nnnGnnn')
+        .aisle('nnnnnnn','nnGGGnn','nGCCCGn','nGC@CGn','nGCCCGn','nnGGGnn','nnnnnnn')
+        .where('@', Predicates.controller(Predicates.blocks(definition.get()))) // コントローラー本体
+        .where('H', Predicates.blocks(GCYMBlocks.CASING_LARGE_SCALE_ASSEMBLING.get())
+          .or(Predicates.abilities(PartAbility.MAINTENANCE).setExactLimit(1)) // メンテ
+          .or(Predicates.abilities(PartAbility.PARALLEL_HATCH).setMaxGlobalLimited(1)) // パラレル
+          .or(Predicates.abilities(PartAbility.DATA_ACCESS, PartAbility.OPTICAL_DATA_RECEPTION).setMaxGlobalLimited(1)) // データ受付
+          .or(Predicates.abilities(PartAbility.IMPORT_ITEMS).setMinGlobalLimited(1).setMaxGlobalLimited(16)) // アイテム搬入
+          .or(Predicates.abilities(PartAbility.IMPORT_FLUIDS).setMinGlobalLimited(1).setMaxGlobalLimited(4)) // 液体搬入
+          .or(Predicates.abilities(PartAbility.EXPORT_ITEMS).setExactLimit(1)) // アイテム搬出
+          .or(Predicates.abilities(PartAbility.INPUT_ENERGY,
+            PartAbility.INPUT_LASER, PartAbility.SUBSTATION_INPUT_ENERGY).setMinGlobalLimited(1).setMaxGlobalLimited(2))) // 電力
+        .where('C', Predicates.blocks(GCYMBlocks.CASING_LARGE_SCALE_ASSEMBLING.get()))
+        .where('G', Predicates.blocks(GTBlocks.CASING_GRATE.get()))
+        .where('h', Predicates.blocks(GTBlocks.COIL_HSSG.get()))
+        .where('L', Predicates.blocks(GTBlocks.CASING_LAMINATED_GLASS.get()))
+        .where('M', Predicates.blocks(GTBlocks.CASING_ASSEMBLY_CONTROL.get()))
+        .where('P', Predicates.blocks(GTBlocks.CASING_STEEL_PIPE.get()))
+        .where('R', Predicates.blocks(GTBlocks.CASING_ASSEMBLY_LINE.get()))
+        .where('V', Predicates.blocks(GCYMBlocks.HEAT_VENT.get()))
+        .where('v', Predicates.blocks(GTBlocks.COMPUTER_HEAT_VENT.get()))
+        .where('W', Predicates.blocks(GTBlocks.SUPERCONDUCTING_COIL.get()))
+        .where('n', Predicates.any())
+        .where(' ', Predicates.air())
+        .build())
+      .workableCasingModel(GTCEu.id('block/casings/gcym/large_scale_assembling_casing'),
+        GTCEu.id('block/multiblock/gcym/large_assembler'))
+    ['tooltips(net.minecraft.network.chat.Component[])']([
+      Component.translatable(outTranslatableString('gtceu', 'machine.available_recipe_map_1'),
+        Component.translatable('gtceu.assembler'), Component.translatable('gtceu.circuit_assembler'),
+        Component.translatable('gtceu.assembly_line')),
+      Component.translatable(outTranslatableString('gtceu', 'multiblock.ffaithial_prpd_assembly_line', 0)),
+      Component.translatable(outTranslatableString('gtceu', 'multiblock.ffaithial_prpd_assembly_line', 1)),
+      Component.translatable(outTranslatableString('gtceu', 'multiblock.ffaithial_prpd_assembly_line', 2)),
+      Component.translatable(outTranslatableString('gtceu', 'multiblock.ffaithial_prpd_assembly_line', 3)),
+      Component.translatable(outTranslatableString('gtceu', 'multiblock.ffaithial_prpd_assembly_line', 4)),
+      Component.translatable(outTranslatableString('gtceu', 'multiblock.ffaithial_prpd_assembly_line', 5)),
+      Component.translatable(outTranslatableString('gtceu', 'multiblock.ffaithial_prpd_assembly_line', 6)),
+    ]);
+  }
+});
